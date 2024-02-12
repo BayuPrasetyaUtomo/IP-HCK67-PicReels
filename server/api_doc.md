e1166850708fc5ee5f6adf85362f0df0c6afeb7e
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-02-12 23:57:04 +0700

    fix: ERD and add UserMoods

---
+++ server/api_doc.md
@@ -0,0 +1,11 @@
+npx sequelize-cli model:generate --name User --attributes username:string,email:string,password:string
+
+npx sequelize-cli model:generate --name Image --attributes title:string,imgUrl:string
+
+npx sequelize-cli model:generate --name Mood --attributes name:string,UserId:integer
+
+npx sequelize-cli model:generate --name MoodImage --attributes ImageId:integer,MoodId:integer
+
+npx sequelize-cli seed:generate --name demo-user
+npx sequelize-cli seed:generate --name demo-image
+npx sequelize-cli seed:generate --name demo-mood
---
+++ server/migrations/20240212161827-create-mood.js
@@ -0,0 +1,38 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Moods', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      UserId: {
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'Users',
+          },
+          key: 'id'
+        },
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Moods');
+  }
+};
